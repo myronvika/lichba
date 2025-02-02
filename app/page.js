a26101afbaff0fc2e75ba914c7dc50af2c7b547f
Vika M <myronvika@gmail.com>
2025-02-02 21:19:48 +0200

    Initial commit from Create Next App

---
+++ app/page.js
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import {Button} from "@/components/ui/button";
+import {Headers} from "next/dist/compiled/@edge-runtime/primitives";
+import Header from "@/app/_components/Header";
+
+export default function Home() {
+    return (
+        <div>
+            <Header/>
+
+        </div>
+    );
+}
